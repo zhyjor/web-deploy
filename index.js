b40999ecf1825c8b1df70ab29a26196c48ac9013
godmeir <zhyjor@163.com>
2018-08-23 11:35:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 
 var apiRoutes = express.Router();
 
-app.use(express.static(path.resolve(__dirname, './dist')))
+app.use(express.static(path.resolve(__dirname, './html/blog/dist')))
 
 app.get('*', function(req, res) {
-    const html = fs.readFileSync(path.resolve(__dirname, './dist/index.html'), 'utf-8')
+    const html = fs.readFileSync(path.resolve(__dirname, './html/blog/dist/index.html'), 'utf-8')
     res.send(html)
 })
 app.listen(80);
